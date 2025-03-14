e88394c603f0f40802dfc5686089d947fca0ff48
mrk19960617 Lienhard <mark.lienhard@kellogg.northwestern.edu>
2025-03-14 04:44:43 +0000

    Change in routes

--- db/migrate/20220309020701_create_users.rb
+++ db/migrate/20220309020701_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string "username"
+      t.string "first_name"
+      t.string "last_name"
       t.string "email"
       t.string "password"
 
@@ -9,3 +10,4 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     end
   end
 end
+
